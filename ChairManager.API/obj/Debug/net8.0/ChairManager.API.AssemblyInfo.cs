5cbeef579f4031b7b8b77652251b4523c47f319a
Vinicius <vini383@gmail.com>
2024-04-29 11:08:34 -0300

    configuration changes

--- ChairManager.API/obj/Debug/net8.0/ChairManager.API.AssemblyInfo.cs
+++ ChairManager.API/obj/Debug/net8.0/ChairManager.API.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1f4f16ec-fa17-43fa-997e-6783f4b22cc1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChairManager.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
